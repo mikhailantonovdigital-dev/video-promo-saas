8998869f0c1b27dc5bbe5303027bc87430bfbbac
mikhailantonovdigital-dev <mikhailantonovdigital@gmail.com>
2026-02-04 11:14:45 +0300

    Update base.py

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -3,3 +3,10 @@ from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
+
+# noqa: F401
+from app.models.user import User
+from app.models.style import Style
+from app.models.plan import Plan
+from app.models.order import Order
+from app.models.payment import Payment
